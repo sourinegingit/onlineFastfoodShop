a71a3df6e493b1589c7969aebc862ac89f540100
negin <neginsouri7979@gmail.com>
2024-12-04 13:19:46 +0430

    header component completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Headre from './components/Headre'
 
 function App() {
 
   return (
     <div className="flex mx-auto bg-gray-300">
       <div className='w-full'>
-        
+        <Headre/>
       </div>
     </div>
   )
---
+++ src/components/Headre.tsx
@@ -0,0 +1,11 @@
+
+const Headre = () => {
+  return (
+    <header className="flex items-center p-4 justify-between bg-orange-600">
+    <h1 className="font-semibold text-lg">🍕 فست فود آنلاین</h1>
+    <p className="font-semibold text-lg">بهترین غذاها در سریع ترین زمان</p>
+  </header>
+  )
+}
+
+export default Headre
